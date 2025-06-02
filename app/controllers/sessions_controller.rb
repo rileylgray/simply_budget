a1d838b42634873d468fc91e574b9363a6513bc7
rileylgray <rileylgray1@outlook.com>
2025-06-02 15:27:08 -0400

    fixed bugs and updated UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
-    puts "helllpooooooooooooooo"
-    puts params[:email]
+
     if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Successfully signed in."
